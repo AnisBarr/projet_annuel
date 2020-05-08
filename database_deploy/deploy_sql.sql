027bec6cb7c2dbfcb740d66f8f8f82a93602227d
AnisBarr <barrma@hotmail.fr>
2020-05-08 18:21:52 +0200

    avancement sur la partie sql

--- database_deploy/deploy_sql.sql
+++ database_deploy/deploy_sql.sql
@@ -3,25 +3,34 @@ CREATE DATABASE IF NOT EXISTS projet_annuel ;
 USE projet_annuel -A;
 
 CREATE TABLE IF NOT EXISTS  user(
-    id_user INT NOT NULL AUTO_INCREMENT,
+   
     nom VARCHAR(100)  NOT NULL,
     prenom VARCHAR(100)  NOT NULL,
-    email VARCHAR(255)  NOT NULL,
+    email VARCHAR(255)  NOT NULL  , 
     password VARCHAR(100) NOT NULL,
     date_naissance DATE  NOT NULL,
     handicap BOOL  NOT NULL,
-    PRIMARY KEY (id_user,email)
+    PRIMARY KEY (email)
     );
 
-CREATE TABLE IF NOT EXISTS  thoadmin(
-    id_user INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS admin(
+
     nom VARCHAR(100)  NOT NULL,
     prenom VARCHAR(100)  NOT NULL,
-    email VARCHAR(255)  NOT NULL,
+    email VARCHAR(255)  NOT NULL  ,
     password VARCHAR(100) NOT NULL,
     date_naissance DATE  NOT NULL,
-    PRIMARY KEY (id_user,email)
+    PRIMARY KEY (email)
+    );
+
+CREATE TABLE IF NOT EXISTS hitory(
+
+    email VARCHAR(255)  NOT NULL  ,
+    type_in_out BOOL NOT NULL,
+    time INT NOT NULL,
+    FOREIGN KEY (email) REFERENCES admin(email)
     );
 
 
-INSERT INTO user (nom,prenom,email,password,date_naissance,handicap)  VALUES ('TEST','TEST','TEST@gmail.com','TEST','2020-02-02',1);
+
+-- INSERT INTO user (nom,prenom,email,password,date_naissance,handicap)  VALUES ('TEST','TEST','TEST@gmail.com','TEST','2020-02-02',1);
